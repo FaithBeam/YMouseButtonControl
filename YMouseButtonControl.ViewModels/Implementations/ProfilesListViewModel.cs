b78376257c14a6be55140baee611f1f28c8fef4e
FaithBeam <pigeonhopper68@gmail.com>
2022-12-12 16:39:06 -0500

    Remove unnecessary notification

--- YMouseButtonControl.ViewModels/Implementations/ProfilesListViewModel.cs
+++ YMouseButtonControl.ViewModels/Implementations/ProfilesListViewModel.cs
@@ -69,6 +69,5 @@ public class ProfilesListViewModel : ViewModelBase, IProfilesListViewModel
             return;
         }
         _profilesService.AddProfile(result);
-        this.RaisePropertyChanged(nameof(Profiles));
     }
 }
