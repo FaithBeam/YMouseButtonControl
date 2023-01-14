9a973aed4b1cde48598dc1c97ee20139142e128d
FaithBeam <pigeonhopper68@gmail.com>
2023-01-14 16:16:38 -0500

    up

--- YMouseButtonControl/DependencyInjection/BackgroundTasksRunner.cs
+++ YMouseButtonControl/DependencyInjection/BackgroundTasksRunner.cs
@@ -9,10 +9,10 @@ namespace YMouseButtonControl.DependencyInjection;
 public static class BackgroundTasksRunner
 {
     private static IMouseListener? _mouseListener;
-    private static KeyboardSimulatorWorker _keyboardSimulatorWorker;
+    private static KeyboardSimulatorWorker? _keyboardSimulatorWorker;
     // private static IPayloadInjectorService _payloadInjectorService;
-    private static ILowLevelMouseHookService _lowLevelMouseHookService;
-    private static ICurrentWindowService _currentWindowService;
+    private static ILowLevelMouseHookService? _lowLevelMouseHookService;
+    private static ICurrentWindowService? _currentWindowService;
     
     public static void Start(IReadonlyDependencyResolver resolver)
     {
@@ -29,9 +29,9 @@ public static class BackgroundTasksRunner
 
     public static void Stop()
     {
-        _currentWindowService.Dispose();
-        _lowLevelMouseHookService.Dispose();
-        _keyboardSimulatorWorker.Dispose();
+        _currentWindowService?.Dispose();
+        _lowLevelMouseHookService?.Dispose();
+        _keyboardSimulatorWorker?.Dispose();
         _mouseListener?.Dispose();
     }
 }
