665986c15f2faa1fc47b2b36c6963bfe7989b2b6
FaithBeam <pigeonhopper68@gmail.com>
2023-01-22 09:45:17 -0500

    Remove unused stuff

--- YMouseButtonControl.Services.Windows/Implementations/Win32Stuff/WinApi.cs
+++ YMouseButtonControl.Services.Windows/Implementations/Win32Stuff/WinApi.cs
@@ -9,9 +9,6 @@ namespace YMouseButtonControl.Services.Windows.Implementations.Win32Stuff;
 public class WinApi
 {
     public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
-    
-    [DllImport("user32.dll")]
-    public static extern bool UnhookWinEvent(IntPtr hWinEventHook);
 
     [DllImport("user32.dll")]
     public static extern IntPtr SetWinEventHook(WinEvents eventMin, WinEvents eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, WinEventFlags dwFlags);
@@ -25,23 +22,8 @@ public class WinApi
     [DllImport("user32.dll")]
     public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam,IntPtr lParam);
     
-    private static IntPtr GetClassLongPtr(IntPtr hWnd, int nIndex)
-    {
-        return IntPtr.Size > 4 ? GetClassLongPtr64(hWnd, nIndex) : new IntPtr(GetClassLongPtr32(hWnd, nIndex));
-    }
-
-    [DllImport("user32.dll", EntryPoint = "GetClassLong")]
-    private static extern uint GetClassLongPtr32(IntPtr hWnd, int nIndex);
-
-    [DllImport("user32.dll", EntryPoint = "GetClassLongPtr")]
-    private static extern IntPtr GetClassLongPtr64(IntPtr hWnd, int nIndex);
-    
-    [DllImport("user32.dll")]
-    private static extern bool GetIconInfo(IntPtr hIcon, out ICONINFO piconinfo);
-    
     [DllImport("user32.dll", SetLastError=true)]
     public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
-
     
     [DllImport("user32.dll", SetLastError = true)]
     public static extern IntPtr GetWindow(IntPtr hWnd, GetWindowType uCmd);
@@ -50,15 +32,6 @@ public class WinApi
     [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool IsWindowVisible(IntPtr hWnd);
     
-    [DllImport("shell32.dll", CharSet=CharSet.Auto)]
-    static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbFileInfo, uint uFlags);
-    
-    [DllImport("user32.dll", SetLastError=true, CharSet=CharSet.Auto)]
-    static extern int GetWindowTextLength(IntPtr hWnd);
-    
-    [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-    static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
-
     [DllImport("kernel32", SetLastError = true)]
     public static extern nint LoadLibrary(string lpFileName);
 
@@ -95,21 +68,6 @@ public class WinApi
     [DllImport("kernel32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool FreeLibrary(IntPtr hModule);
-
-    public IntPtr GetHandleFromProcessId(string pId)
-    {
-        return OpenProcess((uint)(ProcessAccessFlags.All), false,
-            uint.Parse(pId));
-    }
-
-    public bool GetWindowTitleFromHwnd(IntPtr hWnd, out string windowTitle)
-    {
-        var length = GetWindowTextLength(hWnd);
-        var sb = new StringBuilder(length + 1);
-        GetWindowText(hWnd, sb, sb.Capacity);
-        windowTitle = sb.ToString();
-        return sb.Length > 0;
-    }
 
     public string GetBitmapFromPath(string path)
     {
