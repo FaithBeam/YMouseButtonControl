ff875b494910f4ab5933f24b6a60227e43a5e057
FaithBeam <pigeonhopper68@gmail.com>
2023-01-22 11:24:19 -0500

    Function keys

--- YMouseButtonControl.ViewModels/Implementations/Dialogs/SimulatedKeystrokesDialogViewModel.cs
+++ YMouseButtonControl.ViewModels/Implementations/Dialogs/SimulatedKeystrokesDialogViewModel.cs
@@ -15,6 +15,7 @@ public class SimulatedKeystrokesDialogViewModel : DialogBase
     private static readonly Dictionary<string, string> CombinedKeyDict = ModifierKeys
         .Concat(StandardKeys)
         .Concat(DirectionKeys)
+        .Concat(FunctionKeys)
         .ToDictionary(x => x.Key, x => x.Value);
     
     private string _customKeys;
@@ -124,6 +125,34 @@ public class SimulatedKeystrokesDialogViewModel : DialogBase
         { "Down", "{DOWN}" },
         { "Left", "{LEFT}" },
         { "Right", "{RIGHT}" },
+    };
+
+    public static Dictionary<string, string> FunctionKeys => new()
+    {
+        {"F1", "{F1}"},
+        {"F2", "{F2}"},
+        {"F3", "{F3}"},
+        {"F4", "{F4}"},
+        {"F5", "{F5}"},
+        {"F6", "{F6}"},
+        {"F7", "{F7}"},
+        {"F8", "{F8}"},
+        {"F9", "{F9}"},
+        {"F10", "{F10}"},
+        {"F11", "{F11}"},
+        {"F12", "{F12}"},
+        {"F13", "{F13}"},
+        {"F14", "{F14}"},
+        {"F15", "{F15}"},
+        {"F16", "{F16}"},
+        {"F17", "{F17}"},
+        {"F18", "{F18}"},
+        {"F19", "{F19}"},
+        {"F20", "{F20}"},
+        {"F21", "{F21}"},
+        {"F22", "{F22}"},
+        {"F23", "{F23}"},
+        {"F24", "{F24}"}
     };
 
     public string CurrentKey
