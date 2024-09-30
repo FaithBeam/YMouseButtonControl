2f1be3144c81a304faa294a452595de5cf5ef060
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2024-09-30 15:52:06 -0400

    Add YMouseButtonControlDbContextFactory so migrations can occur on macos

--- YMouseButtonControl.DataAccess/Context/YMouseButtonControlDbContext.cs
+++ YMouseButtonControl.DataAccess/Context/YMouseButtonControlDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using YMouseButtonControl.DataAccess.Models;
 
 namespace YMouseButtonControl.DataAccess.Context;
@@ -33,105 +34,112 @@ public class YMouseButtonControlDbContext(DbContextOptions<YMouseButtonControlDb
         modelBuilder
             .Entity<NothingMapping>()
             .HasData(
-                [
-                    new NothingMapping
-                    {
-                        Id = 1,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mb1,
-                    },
-                    new NothingMapping
-                    {
-                        Id = 2,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mb2,
-                    },
-                    new NothingMapping
-                    {
-                        Id = 3,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mb3,
-                    },
-                    new NothingMapping
-                    {
-                        Id = 4,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mb4,
-                    },
-                    new NothingMapping
-                    {
-                        Id = 5,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mb5,
-                    },
-                    new NothingMapping
-                    {
-                        Id = 6,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mwu,
-                    },
-                    new NothingMapping
-                    {
-                        Id = 7,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mwd,
-                    },
-                    new NothingMapping
-                    {
-                        Id = 8,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mwl,
-                    },
-                    new NothingMapping
-                    {
-                        Id = 9,
-                        ProfileId = 1,
-                        MouseButton = MouseButton.Mwr,
-                    },
-                ]
+                new NothingMapping
+                {
+                    Id = 1,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mb1,
+                },
+                new NothingMapping
+                {
+                    Id = 2,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mb2,
+                },
+                new NothingMapping
+                {
+                    Id = 3,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mb3,
+                },
+                new NothingMapping
+                {
+                    Id = 4,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mb4,
+                },
+                new NothingMapping
+                {
+                    Id = 5,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mb5,
+                },
+                new NothingMapping
+                {
+                    Id = 6,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mwu,
+                },
+                new NothingMapping
+                {
+                    Id = 7,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mwd,
+                },
+                new NothingMapping
+                {
+                    Id = 8,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mwl,
+                },
+                new NothingMapping
+                {
+                    Id = 9,
+                    ProfileId = 1,
+                    MouseButton = MouseButton.Mwr,
+                }
             );
         modelBuilder
             .Entity<Profile>()
             .HasData(
-                [
-                    new Profile
-                    {
-                        Id = 1,
-                        Checked = true,
-                        Name = "Default",
-                        IsDefault = true,
-                        Description = "Default description",
-                        Process = "*",
-                        MatchType = "N/A",
-                        ParentClass = "N/A",
-                        WindowCaption = "N/A",
-                        WindowClass = "N/A",
-                    },
-                ]
+                new Profile
+                {
+                    Id = 1,
+                    Checked = true,
+                    Name = "Default",
+                    IsDefault = true,
+                    Description = "Default description",
+                    Process = "*",
+                    MatchType = "N/A",
+                    ParentClass = "N/A",
+                    WindowCaption = "N/A",
+                    WindowClass = "N/A",
+                }
             );
         modelBuilder
             .Entity<SettingBool>()
             .HasData(
-                [
-                    new SettingBool
-                    {
-                        Id = 1,
-                        Name = "StartMinimized",
-                        Value = false,
-                    },
-                ]
+                new SettingBool
+                {
+                    Id = 1,
+                    Name = "StartMinimized",
+                    Value = false,
+                }
             );
         modelBuilder
             .Entity<SettingInt>()
             .HasData(
-                [
-                    new SettingInt
-                    {
-                        Id = 2,
-                        Name = "Theme",
-                        Value = (int)ThemeEnum.Default,
-                    },
-                ]
+                new SettingInt
+                {
+                    Id = 2,
+                    Name = "Theme",
+                    Value = (int)ThemeEnum.Default,
+                }
             );
+    }
+}
+
+public class YMouseButtonControlDbContextFactory
+    : IDesignTimeDbContextFactory<YMouseButtonControlDbContext>
+{
+    public YMouseButtonControlDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<YMouseButtonControlDbContext>();
+        optionsBuilder.UseSqlite(
+            "Data Source=YMouseButtonControlContext-71b0de61-4c45-4680-b3d3-c603b1260ede.db",
+            sqliteOptionsAction: so => so.MigrationsAssembly("YMouseButtonControl.DataAccess")
+        );
+
+        return new YMouseButtonControlDbContext(optionsBuilder.Options);
     }
 }
