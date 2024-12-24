f459b90072af09c34966e62910fc3353a7f82a0d
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2024-12-24 14:41:22 -0500

    Profiles information

--- YMouseButtonControl.Core/ViewModels/ProfilesInformationViewModel/ProfilesInformationViewModel.cs
+++ YMouseButtonControl.Core/ViewModels/ProfilesInformationViewModel/ProfilesInformationViewModel.cs
@@ -1,15 +1,33 @@
-﻿using YMouseButtonControl.Core.Services.Profiles;
+﻿using ReactiveUI;
+using YMouseButtonControl.Core.Services.Profiles;
 
 namespace YMouseButtonControl.Core.ViewModels.ProfilesInformationViewModel;
 
-public interface IProfilesInformationViewModel
+public interface IProfilesInformationViewModel { }
+
+public class ProfilesInformationViewModel : ViewModelBase, IProfilesInformationViewModel
 {
-    public IProfilesCache ProfilesService { get; }
+    private IProfilesCache ProfilesCache { get; }
+    private readonly ObservableAsPropertyHelper<ProfilesInformationModel> _profileInfo;
+    public ProfilesInformationModel ProfileInfo => _profileInfo.Value;
+
+    public ProfilesInformationViewModel(IProfilesCache profilesCache)
+    {
+        ProfilesCache = profilesCache;
+        _profileInfo = this.WhenAnyValue(
+                x => x.ProfilesCache.CurrentProfile,
+                selector: vm => new ProfilesInformationModel(vm)
+            )
+            .ToProperty(this, x => x.ProfileInfo);
+    }
 }
 
-public class ProfilesInformationViewModel(IProfilesCache profilesService)
-    : ViewModelBase,
-        IProfilesInformationViewModel
+public class ProfilesInformationModel(Models.ProfileVm? vm)
 {
-    public IProfilesCache ProfilesService { get; } = profilesService;
+    public string Description { get; } = vm?.Description ?? "N/A";
+    public string WindowCaption { get; } = vm?.WindowCaption ?? "N/A";
+    public string Process { get; } = vm?.Process ?? "N/A";
+    public string WindowClass { get; } = vm?.WindowClass ?? "N/A";
+    public string ParentClass { get; } = vm?.ParentClass ?? "N/A";
+    public string MatchType { get; } = vm?.MatchType ?? "N/A";
 }
